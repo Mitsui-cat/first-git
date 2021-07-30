38f5d379949ba64a4ea26cf59a55d9ba5a2cdf80
cx <cx@qq.com>
2021-07-30 20:56:26 +0800

    first commit

---
+++ src/main/java/com/cx/GitTest.java
@@ -0,0 +1,13 @@
+package com.cx;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author CX
+ * @create 2021-07-30 20:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git-test1");
+    }
+}
