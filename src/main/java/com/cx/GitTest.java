a381cf5e6b0c8e495c11d1f1ee0d25e898f2d54e
cx <cx@qq.com>
2021-07-30 20:57:49 +0800

    second commit

--- src/main/java/com/cx/GitTest.java
+++ src/main/java/com/cx/GitTest.java
@@ -9,5 +9,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git-test1");
+        System.out.println("git-test2");
+
     }
 }
